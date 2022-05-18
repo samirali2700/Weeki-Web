6f190b1769aa1f3c380542955260fb7cfdc8284f
samirali2700 <samirali@live.dk>
2022-05-18 10:44:38 +0200

    fix port

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.get('*', (req,res) => {res.redirect('/')})
 
 
 
-server.listen(3000 || process.env.PORT, () => {
+server.listen(process.env.PORT || 3000 , () => {
     console.log('app listening on port', server.address().port);
 })
